0bcb26c83a4fb2feb107b18354fa040ea698a72d
pabloszar <psp301098@gmail.com>
2023-01-20 00:33:10 -0600

    Test 6 aprobado

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -1,20 +1,27 @@
 // Do not change any of the function names
 
+const { join } = require("@11ty/eleventy/src/TemplatePath");
+
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
-  //Tu código:
+  //Tu código: 
+    rName = Array.from(nombre);
+    rName[0] = nombre[0].toUpperCase();
+    return(rName.join(''));
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return(cb(n1, n2));
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +29,21 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  suma = 0;
+  numeros.forEach(t => {
+    if(t == Math.round(t)){
+      suma += t;
+    }
+  })
+
+  cb(suma);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(t => {cb(t);})
 }
 
 function map(array, cb) {
@@ -35,12 +51,22 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  const nuevoArray = [];
+  array.map(t => {nuevoArray.push(cb(t))});
+  return(nuevoArray);
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  const newArray = [];
+  array.forEach(t => {
+    if (t[0] == "a") {
+      newArray.push(t);
+    }
+  });
+  return(newArray);
 }
 
 // No modificar nada debajo de esta línea
